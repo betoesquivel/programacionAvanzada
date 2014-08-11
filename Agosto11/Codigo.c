057dc46aea457df60348d845ca41b95b84729a83
Beto Esquivel <beto.esquivel.p@gmail.com>
2014-08-11 16:17:06 -0500

    Not the best code, it is just code that works, but it is not parallel or optimized for our processor (CPU)

---
+++ Agosto11/Codigo.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Codigo.c
+ *
+ *    Description:  ejemplo en clase de introduccion
+ *
+ *        Version:  1.0
+ *        Created:  08/11/2014 04:12:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jose Alberto Esquivel Patino (), 
+ *   Organization:  ITESM Campus Monterrey
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h> // Tiene el prototipo de las funciones que voy a usar
+// Un prototipo incluye el nombre, cantidad de parametros y tipos de parametros.
+
+void main() {
+	printf("Hola\n" ); 
+}
